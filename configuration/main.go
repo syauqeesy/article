1a17c9c0750cab22f9a5f51fc5680bc90555ed20
Ahmad Syauqi <syauqeesy@gmail.com>
2026-02-12 17:10:49 +0700

    remove unused config

--- configuration/main.go
+++ configuration/main.go
@@ -26,7 +26,6 @@ type Configuration struct {
 	}
 	Authentication struct {
 		Secret        string `json:"secret"`
-		RefreshSecret string `json:"refresh_secret"`
 		RefreshPepper string `json:"refresh_pepper"`
 	}
 	Application struct {
