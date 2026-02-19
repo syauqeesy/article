cb860414f3ac6736c9d0d0177577bf7d02693c15
Ahmad Syauqi <syauqeesy@gmail.com>
2026-02-19 19:17:38 +0700

    remove unused exception

--- exception/http.go
+++
@@ -1,9 +0,0 @@
-package exception
-
-import (
-	"net/http"
-
-	"ahmadsyauqi.dev/article/common"
-)
-
-var TooManyRequests = common.CreateException(http.StatusTooManyRequests, http.StatusText(http.StatusTooManyRequests))
